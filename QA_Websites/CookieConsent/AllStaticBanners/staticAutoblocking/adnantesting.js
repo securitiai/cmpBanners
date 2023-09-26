b1d48fc1112ea44805e575b72e93a77c38ad6682
Mohit Choudhary <mohit.choudhary@securiti.ai>
2023-09-26 15:27:12 +0530

    qa-stage autoblokcing

--- QA_Websites/CookieConsent/AllStaticBanners/staticAutoblocking/adnantesting.js
+++
@@ -1,3 +0,0 @@
-console.assert(false,"Cookie3 and Unpublish feature toggle is disable on Portal and cookie will not be drop drop");
-document.cookie = "Cookie3=Unpublished"
-
---
+++ QA_Websites/CookieConsent/AllStaticBanners/staticAutoblocking/cookie3.js
@@ -0,0 +1,2 @@
+console.assert(false,"Cookie3 and Unpublished and this cookie should not drop");
+document.cookie = "Cookie3=Unpublished"
