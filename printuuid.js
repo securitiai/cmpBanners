90e64149d93e30dd8a2e67c6cfe9975181535379
mohitchoudharysecuritiai <mohit.choudhary@securiti.ai>
2024-07-11 22:49:28 +0530

    print uuid

---
+++ printuuid.js
@@ -0,0 +1,25 @@
+(function() {
+    var originalOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+      this.addEventListener('readystatechange', function() {
+          if (this.readyState === 4 && this.responseURL.includes('singleupload')) { // Request is done
+              var jsonString = this._body;
+              var jsonObject = JSON.parse(jsonString);
+              var uuid = jsonObject.uuid;
+              console.assert(false,"uuid=>"+uuid);
+              var domain_id = jsonObject.domain_id;
+              console.log("domain_id=>"+domain_id);
+              var policy_version = jsonObject.policy_version;
+              console.log("policy_version=>"+policy_version);
+          }
+      }, false);
+    
+      originalOpen.apply(this, arguments);
+    };
+    
+    var originalSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function(body) {
+      this._body = body;
+      originalSend.apply(this, arguments);
+    };
+    })();
