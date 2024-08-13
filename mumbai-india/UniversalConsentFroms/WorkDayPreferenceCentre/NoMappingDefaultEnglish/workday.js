303850c3f15aaa8345e0a5987db55803a2b79b8f
mohitchoudharysecuritiai <mohit.choudhary@securiti.ai>
2024-08-13 17:00:47 +0530

    banners

---
+++ mumbai-india/UniversalConsentFroms/WorkDayPreferenceCentre/NoMappingDefaultEnglish/workday.js
@@ -0,0 +1,84 @@
+const PUB = window.postMessage;
+const SUB = window.addEventListener;
+
+// Iframe Configuration to render preference center.
+const CREDENTIALS = {
+    tenantId: "app-eu~0f42fdb1-6107-4121-955b-8153f234d9b7",
+    preferenceCenterId: "80fded07-cbbd-4da2-a6be-7958f4080c0c",
+    primaryIdentifier: '',
+    locationCode: 'default',
+    languageCode: 'en',
+    implicitFlow: true,
+    implicitRecordConsents: false,
+    allMandatory: true
+};
+
+(function () {
+    //? STEP 1: subscribe to window events
+    SUB("message", handleWindowEvents);
+
+    //? STEP 2: Get the iframe ref and add an onload event listener.
+    // As and when iframe loaded(i.e, preference center) workday can send the information that iframe needs
+    // in order to load the consents.
+    const preferenceIframeRef = document.getElementById("preferenceIframe");
+
+    preferenceIframeRef.addEventListener("load", (ev) => {
+        //? STEP 3: Load the config into iframe/sdk.
+        loadConfigIntoIframe()
+    });
+
+    //? STEP 4: Attach a listener to the primary identifier and let the iframe know about the change.
+    const primaryIdentifierRef = document.getElementById('email');
+    primaryIdentifierRef.addEventListener('input', handlePrimaryIdChange);
+
+    // primary identifier already provided then update ui
+    if (CREDENTIALS.primaryIdentifier) {
+        primaryIdentifierRef.value = CREDENTIALS.primaryIdentifier;
+    }
+
+    // Load config into iframe.
+    function loadConfigIntoIframe() {
+        //? Step 2: Load Credentials into iframe.
+        PUB(
+            {
+                type: "INIT_PREFERENCE_CENTER",
+                config: CREDENTIALS
+            },
+            '*'
+        );
+    }
+
+    // Subscribing iframe events.
+    function handleWindowEvents(evt) {
+        const evtType = getEventType(evt);
+
+        // CONSENTS_PROVIDED EVENT 
+        // fired when user clicks on submit.
+        if (evtType.toUpperCase() === 'CONSENTS_PROVIDED') {
+            PUB({
+                type: "RECORD_CONSENTS",
+                value: true
+            }, '*');
+        }
+    }
+
+    // Update primary identifer in sdk.
+    function handlePrimaryIdChange(ev) {
+        const value = ev.target.value;
+        PUB({
+            type: 'SET_PRIMARY_IDENTIFIER',
+            value: value
+        }, '*')
+    }
+
+    // To unsubscribe window events.
+    function unsubscribeWindowEvents() {
+        window.removeEventListener('message', handleWindowEvents);
+    }
+    /** END OF OPTIONAL STEPS */
+
+    function getEventType(evt) {
+        return evt.data.type || "UNKNOWN";
+    }
+
+})();
---
+++ mumbai-india/UniversalConsentFroms/eu_workdayInfo.js
@@ -0,0 +1,22 @@
+// ************* Info *****************
+
+// NoMappingBrazilEnglish 
+
+// https://mohitchoudhary747.github.io/cmpBanners/ProdEU/UniversalConsentFroms/WorkDayPreferenceCentre/NoMappingBrazilEnglish/index.html
+
+// NoMappingBrazilPortuguese_Br
+// https://mohitchoudhary747.github.io/cmpBanners/ProdEU/UniversalConsentFroms/WorkDayPreferenceCentre/NoMappingBrazilPortuguese_Br/index.html
+
+// NoMappingDefaultEnglish
+// https://mohitchoudhary747.github.io/cmpBanners/ProdEU/UniversalConsentFroms/WorkDayPreferenceCentre/NoMappingDefaultEnglish/index.html
+
+// NoMappingDefaultSpanish
+// https://mohitchoudhary747.github.io/cmpBanners/ProdEU/UniversalConsentFroms/WorkDayPreferenceCentre/NoMappingDefaultSpanish/index.html
+
+// WithSpanishMappingFranceEnglish
+// https://mohitchoudhary747.github.io/cmpBanners/ProdEU/UniversalConsentFroms/WorkDayPreferenceCentre/WithSpanishMappingFranceEnglish/index.html
+
+// WithSpanishMappingFranceSpanish
+// https://mohitchoudhary747.github.io/cmpBanners/ProdEU/UniversalConsentFroms/WorkDayPreferenceCentre/WithSpanishMappingFranceSpanish/index.html
+
+//*[text()='Select Language']/following-sibling::*//*[text()]
